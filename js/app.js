9a0748f63847ca0e30d44262f4e6b09548b20d3d
Lindsay M <lminami23@punahou.edu>
2018-05-26 11:02:15 -1000

    POKEMON!

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 var player = document.getElementById('player');
-var location = document.getElementById('location');
+var loca = document.getElementById('location');
+var modal = document.getElementById('modal-container');
+var img = document.getElementById('modal-img');
+var pokeName = document.getElementById('modal-name');
 var playerTop = 0;
 var playerLeft = 0;
 var counter = 1;
+var arr = []
 
 
 document.addEventListener('keydown', function(e){
@@ -55,10 +59,37 @@ function generateScene(){
 		if (xhr.readyState == 4 && xhr.status == 200){
 			//console.log(xhr)
 			var response = JSON.parse(xhr.responseText);
-			console.log(response.name);
-			location.innerHTML = response.name
+			console.log(response);
+			loca.innerHTML = "Location: " + response.name
+			arr = response.pokemon_encounters
+			generatePokemon()
 		}
 	}
 }
 
 generateScene()
+
+function generatePokemon(){
+	//console.log(arr)
+	var randomNum = Math.floor(Math.random() * arr.length);
+	//console.log(randomNum);
+	console.log(arr[randomNum].pokemon.url)
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', arr[randomNum].pokemon.url, true);
+	xhr.send();
+	xhr.addEventListener("readystatechange", processReq, false);
+
+	function processReq(e){
+		if (xhr.readyState == 4 && xhr.status == 200){
+			var response = JSON.parse(xhr.responseText);
+			console.log(response.sprites.front_default);
+			console.log(response.name);
+			modal.style.display = 'flex';
+		img.src = response.sprites.front_default;
+		img.style.height = '220px'
+		img.style.width = '220px'
+		pokeName.innerHTML = response.name
+		}
+	}
+}
